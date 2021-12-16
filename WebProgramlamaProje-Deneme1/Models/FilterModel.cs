0bd27b9da1bddbdf78864be17391675e56074fa5
Salih <salih.esme@hotmail.com>
2021-12-16 13:26:44 +0300

    FiterModelAdded

---
+++ WebProgramlamaProje-Deneme1/Models/FilterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgramlamaProje_Deneme1.Models
+{
+    public class FilterModel
+    {
+        public string Brand { get; set; }
+        public string FuelType{ get; set; }
+        public string TypeOfGear { get; set; }
+        public uint DailyPrice { get; set; }
+        public int BranchId { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime DeliverDate { get; set; }
+    }
+}
